619ce1420ef1fec93012ccd874af6f0f62653b5e
Angel Henriquez <angelhm20@hotmail.com>
2025-04-21 20:32:52 -0400

    Warn the user when the pledge money amount is less than expected

--- src/js/index.js
+++ src/js/index.js
@@ -30,6 +30,11 @@ const pledgeOptionsTitle = document.querySelectorAll(".pledge-option-title");
 const pledgeRadioInputs = document.querySelectorAll(".pledge-radio");
 const pledgeOptions = document.querySelectorAll(".modal-form-option");
 const submitFormDiv = document.querySelectorAll('.submit-form-div');
+const pledgeForm = document.getElementById('pledge-form');
+const submitBtn = document.querySelectorAll('.submit-btn');
+const moneyAmountInput = document.querySelectorAll('.money-amount');
+const pledgeAmountWrn = document.querySelectorAll('.pledge-amount-wrn');
+const minPledgeSign = document.querySelectorAll('.min-pledge-form');
 
 const openModal = () => {
     modal.showModal();
@@ -72,6 +77,70 @@ for (let i = 0; i < pledgeRadioInputs.length; i++) {
         if (pledgeRadioInputs[i].checked) {
             pledgeOptions[i].style.borderColor = '#3CB4AB';
             submitFormDiv[i].classList.add('checked-radio');
+        }
+
+        for (let j = 0; j < pledgeAmountWrn.length; j++) {
+            if (!pledgeAmountWrn[j].classList.contains('hidden')) {
+                pledgeAmountWrn[j].classList.add('hidden');
+                pledgeOptions[j].style.borderColor = '';
+                radioSpans[j].classList.remove('active-wrn');
+                submitBtn[j].style.backgroundColor = '';
+                moneyAmountInput[j].value = '';
+                
+                if (j > 0) {
+                    minPledgeSign[j - 1].classList.remove('wrn-animation');
+                }
+            }
+        }
+    });
+}
+
+// Money amount warning message if is less than expected
+pledgeForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    moneyAmountInput.forEach(input => {
+        if (input.value) {
+            if (input.name === "money-amount-any") {
+                if (input.value < 1) {
+                    pledgeAmountWrn[0].classList.remove('hidden');
+                    pledgeOptions[0].style.borderColor = 'red';
+                    radioSpans[0].classList.add('active-wrn');
+                    submitBtn[0].style.backgroundColor = 'red';
+                }
+            } else if (input.name === "money-amount-25") {
+                if (input.value < 25) {
+                    pledgeAmountWrn[1].classList.remove('hidden');
+                    pledgeOptions[1].style.borderColor = 'red';
+                    radioSpans[1].classList.add('active-wrn');
+                    submitBtn[1].style.backgroundColor = 'red';
+                    minPledgeSign[0].classList.add('wrn-animation');
+                }
+            } else if (input.name === "money-amount-75") {
+                if (input.value < 75) {
+                    pledgeAmountWrn[2].classList.remove('hidden');
+                    pledgeOptions[2].style.borderColor = 'red';
+                    radioSpans[2].classList.add('active-wrn');
+                    submitBtn[2].style.backgroundColor = 'red';
+                    minPledgeSign[1].classList.add('wrn-animation');
+                }
+            }
+        }
+    });
+});
+
+// Return all this elements to default state when input value changes
+for (let i = 0; i < moneyAmountInput.length; i++) {
+    moneyAmountInput[i].addEventListener('input', () => {
+        if (!pledgeAmountWrn[i].classList.contains('hidden')) {
+            pledgeAmountWrn[i].classList.add('hidden');
+            pledgeOptions[i].style.borderColor = '';
+            radioSpans[i].classList.remove('active-wrn');
+            submitBtn[i].style.backgroundColor = '';
+            
+            if (i > 0) {
+                minPledgeSign[i - 1].classList.remove('wrn-animation');
+            }
         }
     });
 }
